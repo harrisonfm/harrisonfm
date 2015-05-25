b62aa82db6d27ed73b55f4acdd6d27adf682bfe4
Harrisonfm <john@harrisonfm.com>
2015-05-25 19:50:38 -0400

    Dynamic About

--- app/js/views/About.js
+++ app/js/views/About.js
@@ -4,6 +4,7 @@ define([
     'backbone',
     'views/Page',
     'views/Nav',
+    'views/Loader',
     'tweenmax',
     'templates'
 ], function($,
@@ -11,6 +12,7 @@ define([
     Backbone,
     PageView,
     NavView,
+    LoaderView,
     TweenMax,
     JST) {
 
@@ -18,10 +20,6 @@ define([
 
     var AboutView = PageView.extend({
         className: 'About page',
-        template: JST['app/js/templates/About.ejs'],
-
-        events: {
-        },
 
         /* ----------------------------------------------------------------------------- *\
            Public Methods
@@ -35,6 +33,10 @@ define([
         initialize: function(opts) {
             PageView.prototype.initialize.apply(this, arguments);
             this.navView = new NavView();
+            this.loaderView = new LoaderView();
+            this.getAuthorInfo();
+            _.bindAll(this, 'loadingDone');
+            Backbone.pubSub.on('loadingDone', this.loadingDone);
         },
 
         /**
@@ -43,14 +45,30 @@ define([
         @return {AboutView}
         **/
         render: function() {
-            this.$el.html(this.template());
+            this.$el.html();
             this.$el.prepend(this.navView.render().el);
-
+            this.$el.append(this.loaderView.render().el);
             return this;
         },
 
-        doneTransitioning: function(){
-            var objects = [this.navView.el, this.$('.about-container')[0], [this.$('footer')[0], this.$('.me')[0]]];
+        getAuthorInfo: function() {
+            $.getJSON(Backbone.api, {
+                    json: 'get_author_index',
+                    author_meta: 'twitter,facebook,spotify,steam,instagram,author_profile_picture,user_email'
+                },
+                function(response){
+                    var author = response.authors[0];
+                    var append = '<div class="about-container"><img id="me" src="'+author.author_profile_picture+'">';
+                    append += '<section>'+author.description.replace(/[\n\r]/g, '<br>')+'</section>';
+                    append += '<footer><a href="mailto:'+author.user_email+'">E-mail</a> <a href="'+author.instagram+'" target="_blank">Instagram</a> <a href="'+author.spotify+'" target="_blank">Spotify</a> <a href="'+author.twitter+'" target="_blank">Twitter</a> <a href="'+author.facebook+'" target="_blank">Facebook</a> <a href="'+author.steam+'" target="_blank">Steam</a></footer></div>';
+                    this.loaderView.startLoad(0);
+                    this.$el.append(append);
+                }.bind(this)
+            );
+        },
+
+        loadingDone: function(){
+            var objects = [this.navView.el, this.$('.about-container')[0], [this.$('footer')[0], this.$('#me')[0]]];
             TweenMax.staggerTo(objects, 0.5, {opacity: 1}, 0.5);
         }
     });
--- hfmapi/wp-content/plugins/internal.php
+++ hfmapi/wp-content/plugins/internal.php
@@ -2,6 +2,8 @@
 /*
 Plugin Name: Internal Mods
 Description: Internal PHP
+
+JSON Api modified to allow user_email visibility
 */
 
 add_image_size('wide', 600, 600);
@@ -14,4 +16,56 @@ function customFormatGallery($string,$attr){
     	$output .= wp_get_attachment_image($imagePost->ID, 'wide');
     }
     return $output;
+}
+
+add_action('show_user_profile', 'my_show_extra_profile_fields');
+add_action('edit_user_profile', 'my_show_extra_profile_fields');
+
+function my_show_extra_profile_fields( $user ) { ?>
+	<h3>Extra profile information</h3>
+	<table class="form-table">
+		<tr>
+			<th><label for="twitter">Twitter</label></th>
+			<td>
+				<input type="text" name="twitter" id="twitter" value="<?php echo esc_attr(get_the_author_meta('twitter', $user->ID)) ?>" class="regular-text" />
+			</td>
+		</tr>
+		<tr>
+			<th><label for="instagram">Instagram</label></th>
+			<td>
+				<input type="text" name="instagram" id="instagram" value="<?php echo esc_attr(get_the_author_meta('instagram', $user->ID)) ?>" class="regular-text" />
+			</td>
+		</tr>
+		<tr>
+			<th><label for="spotify">Spotify</label></th>
+			<td>
+				<input type="text" name="spotify" id="spotify" value="<?php echo esc_attr(get_the_author_meta('spotify', $user->ID)) ?>" class="regular-text" />
+			</td>
+		</tr>
+		<tr>
+			<th><label for="facebook">Facebook</label></th>
+			<td>
+				<input type="text" name="facebook" id="facebook" value="<?php echo esc_attr(get_the_author_meta('facebook', $user->ID)) ?>" class="regular-text" /> 
+			</td>
+		</tr>
+		<tr>
+			<th><label for="steam">Steam</label></th>
+			<td>
+				<input type="text" name="steam" id="steam" value="<?php echo esc_attr(get_the_author_meta('steam', $user->ID)) ?>" class="regular-text" /> 
+			</td>
+		</tr>
+	</table>
+<?php }
+
+add_action('personal_options_update', 'my_save_extra_profile_fields');
+add_action('edit_user_profile_update', 'my_save_extra_profile_fields');
+ 
+function my_save_extra_profile_fields($user_id){
+    if (!current_user_can('edit_user', $user_id))
+        return false;
+    update_user_meta($user_id, 'twitter', $_POST['twitter']);
+    update_user_meta($user_id, 'spotify', $_POST['spotify']);
+    update_user_meta($user_id, 'facebook', $_POST['facebook']);
+    update_user_meta($user_id, 'steam', $_POST['steam']);
+    update_user_meta($user_id, 'instagram', $_POST['instagram']);
 }
